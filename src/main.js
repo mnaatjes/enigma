7ff64ed2f51954ec6ddd0186e0b01cd599f112cc
mnaatjes <michael.naatjes87@gmail.com>
2025-05-21 13:00:54 -0400

    feat: Enhance documentation for render and buildConfig functions with detailed parameter descriptions

--- src/main.js
+++ src/main.js
@@ -8,11 +8,21 @@
  */
 
 import { AbstractElement } from "./custom_elements/AbstractElement.js";
-
+/*----------------------------------------------------------*/
 /**
+ * Defines and Creates Custom HTML Component from AbstractElement and appends to DOM
  * @function render
- * @param {object} configuration
+ * 
+ * @param {{
+ *      tag_name: string,
+ *      parent: HTMLElement,
+ *      props: object
+ *      content: function
+ * }} configuration
+ * 
+ * @returns {void} Renders and Mounts custom web component to DOM
  */
+/*----------------------------------------------------------*/
 function render(configuration){
     /**
      * Declare "config" as constant
@@ -31,6 +41,52 @@ function render(configuration){
      */
     config.parent.appendChild(document.createElement(config.tag_name));
 }
+/*----------------------------------------------------------*/
+/**
+ * @function buildConfig
+ * Custom Element Configuration Factory:
+ * - Validate tag_name prop
+ * - Validate parent prop
+ * - Build and append props object
+ * - Generate content method
+ * 
+ * @param {string} tag_name
+ * @param {HTMLElement} parent
+ * @param {string} content
+ * @param {object} properties
+ * 
+ * @returns {{
+ *      tag_name: string,
+ *      parent: HTMLElement,
+ *      props: object
+ *      content: function
+ * }}
+ */
+/*----------------------------------------------------------*/
+function buildConfig(tag_name, parent, content, properties={}){
+    /**
+     * Validate tag_name
+     */
+    /**
+     * Validate parent
+     */
+    /**
+     * Validate Content
+     */
+    /**
+     * Check properties Exist:
+     */
+    /**
+     * Generate Content function and append properties
+     */
+    /**
+     * Return results object
+     */
+
+}
+
+const config = buildConfig('invalidName', document.getElementById('invalid'), 12, {});
+
 
 /**
  * @type {object}
@@ -45,12 +101,3 @@ const element_config = {
     content: function(instance){return `<b>${instance.title}</b>`;}
 };
 render(element_config);
-
-/**
- * Testing
- */
-/*
-const obj = {a: 1, b: 2};
-console.log(obj);
-console.log("Call", Object.prototype.hasOwnProperty.call(obj, 'toString'));
-*/
