4686bf6189333b8366dcf3b0ca35f213aba172d2
mnaatjes <michael.naatjes87@gmail.com>
2025-08-25 12:55:26 -0400

    init restructure push

---
+++ src/components/Ringstellung.js
@@ -0,0 +1,10 @@
+/**
+ * @class Ringstellung
+ * @file src/components/Ringstellung.js
+ * 
+ * @since 1.0.0:
+ * - Created
+ * 
+ * 
+ * @version 1.0.0
+ */
---
+++ src/config.js
@@ -0,0 +1,10 @@
+/**
+ * Configuration constants for initial Enigma settings
+ * 
+ * @file src/config.js
+ * @since 1.0.0:
+ * - Created
+ * 
+ * 
+ * @version 1.0.0
+ */
